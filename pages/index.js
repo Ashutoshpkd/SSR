1bd48bf80d0311589b61dffee0b35276fcf68fde
Ashutosh Pawade <ashpawad@WKMZTE2DB86B.global.publicisgroupe.net>
2022-11-18 12:02:01 +0530

    Image lazy loading

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,9 @@ import React from "react";
 import Head from "next/head";
 import Link from "next/link";
 import styles from "../styles/Home.module.css";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
+import "react-lazy-load-image-component/src/effects/opacity.css";
 
 export async function getServerSideProps() {
   const resp = await fetch(
@@ -28,9 +31,13 @@ export default function Home({ pokemon }) {
           <div className={styles.card} key={pokemon.id}>
             <Link href={`/pokemon/${pokemon.id}`}>
               <a>
-                <img
+                <LazyLoadImage
                   src={`https://ssr-pokemon-dev.s3.amazonaws.com/${pokemon.image}`}
                   alt={pokemon.name}
+                  height="200px"
+                  width="215px"
+                  effect="blur"
+                  placeholderSrc={`https://ssr-pokemon-dev.s3.amazonaws.com/${pokemon.image}`}
                 />
                 <h3>{pokemon.name}</h3>
               </a>
